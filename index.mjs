ac73655a6354d02287d98669fe3c5db5363cadd2
Valdinei Santos <valdinei.vs@gmail.com>
2025-02-13 16:02:54 -0300

    Ajustes para testar github actions

--- index.mjs
+++ index.mjs
@@ -5,6 +5,6 @@ export const handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify(event),
     };
-    log('log via handler. event: ', JSON.stringify(event));
+    log('log com github actions: ', JSON.stringify(event));
     return response;
   };
--- log.mjs
+++ log.mjs
@@ -1,3 +1,3 @@
 export function log(mensagem) {
-    console.log('log via função', mensagem);
+    console.log('log via função: ', mensagem);
 }
